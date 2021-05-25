4282f4a92cdbee3bcd8a04f5ab65be88ff26912b
Topias Mariapori <topias@mariapori.fi>
2021-05-26 00:35:13 +0300

    Automaattinen osumien tarkistus

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import requests
 import datetime
 import json
 import os
+import sys
+
+osumat = 0
+veikatut = []
+
+if len(sys.argv) > 1:
+    tiedosto = open(sys.argv[1],"r")
+    lines = tiedosto.readlines()
+    for line in lines:
+        veikatut.append(line.split(" "))
+
 year = datetime.date.today().year
 week = "W" + str(datetime.date.today().isocalendar()[1])
 thisweekandyear = str(year) + "-" + week
@@ -16,10 +27,19 @@ print("\r\n")
 if len(data) > 0:
     for item in data[0]["results"][0]["primary"]:
         print("("+item+")", end=" ")
+        if item in veikatut[0]:
+            osumat = osumat + 1
+
     print("\r\n")
     print("Lisänumero: " + "("+data[0]["results"][0]["secondary"][0]+")")
+    if data[0]["results"][0]["secondary"][0] in veikatut[0]:
+        osumat = osumat +1
     print("Plus-numero: " + "("+data[0]["results"][0]["tertiary"][0]+")")
+    if data[0]["results"][0]["tertiary"][0] in veikatut[0]:
+        osumat = osumat +1
     print("\r\n")
+    if osumat > 0:
+        print("Osumia: " + str(osumat))
 else:   
     print("Tällä viikolla ei ole vielä arvottu lottoa.")
 print("*** By Topias Mariapori ***")
